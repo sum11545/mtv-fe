b002e7d46e4226c24cb8cfae311f99732792b016
jyotixg <jyoti.gupta@texple.com>
2025-05-27 15:49:33 +0530

    fixed continous api call issue

--- src/pages/[section].js
+++ src/pages/[section].js
@@ -27,7 +27,7 @@ export default function SectionPage() {
     };
 
     loadData();
-  }, [fetchSectionPageData]);
+  }, []);
 
   return (
     <Layout>
--- src/pages/index.js
+++ src/pages/index.js
@@ -23,7 +23,7 @@ export default function Home() {
     };
 
     loadData();
-  }, [fetchHomePageData]);
+  }, []);
 
   return (
     <Layout>
