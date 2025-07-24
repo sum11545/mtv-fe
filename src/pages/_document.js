19213c63ab42ad6de0e937d035d35a7f8e5fd2f8
jyotixg <jyoti.gupta@texple.com>
2025-07-24 12:42:21 +0530

    Added google tag manager script

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -9,9 +9,23 @@ const defaultTheme = createAppTheme("light");
 
 export default class MyDocument extends Document {
   render() {
+    const GTM_ID = process.env.NEXT_PUBLIC_GTM_ID;
+
     return (
       <Html lang="en">
         <Head>
+          {/* Google Tag Manager */}
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','${GTM_ID}');`,
+            }}
+          />
+          {/* End Google Tag Manager */}
+
           {/* Open Graph Meta Tags */}
           <meta
             property="og:title"
@@ -132,6 +146,14 @@ export default class MyDocument extends Document {
           {this.props.emotionStyleTags}
         </Head>
         <body>
+          {/* Google Tag Manager (noscript) */}
+          <noscript
+            dangerouslySetInnerHTML={{
+              __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=${GTM_ID}"
+      height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+            }}
+          />
+
           <Main />
           <NextScript />
         </body>
